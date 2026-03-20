7d740818ee3d44196bf875201e52b39f2ea5c509
Gauthier Doppler <gauthier.doppler@theodo.com>
2026-03-20 14:30:19 +0100

    feat: add fff.nvim for grep, keep telescope for file finding

---
+++ lua/custom/plugins/fff.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      require('fff.download').download_or_build_binary()
+    end,
+    lazy = false,
+    opts = {
+      keymaps = {
+        cycle_grep_modes = '<S-Tab>',
+      },
+      grep = {
+        modes = { 'plain', 'regex', 'fuzzy' },
+      },
+    },
+    config = function(_, opts)
+      require('fff').setup(opts)
+
+      local ok, grep_renderer = pcall(require, 'fff.grep.grep_renderer')
+      if not ok or grep_renderer._blob_guard_patched then return end
+
+      local original_render_line = grep_renderer.render_line
+      grep_renderer.render_line = function(item, ctx)
+        if item and type(item.line_content) == 'string' then
+          -- NUL bytes trigger Vim:E976 in strdisplaywidth (treated as Blob).
+          item.line_content = item.line_content:gsub('%z', ' ')
+        end
+        return original_render_line(item, ctx)
+      end
+
+      grep_renderer._blob_guard_patched = true
+
+      local function get_prompt_and_query()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local prompt = vim.fn.prompt_getprompt(bufnr) or ''
+        local line = vim.api.nvim_get_current_line()
+        local query = line
+        if prompt ~= '' and line:sub(1, #prompt) == prompt then query = line:sub(#prompt + 1) end
+        return prompt, query
+      end
+
+      -- <C-i> appends folder scope constraint: `pattern **/**` with cursor between the slashes
+      local add_folder_scope = function()
+        local ok_picker, picker_ui = pcall(require, 'fff.picker_ui')
+        if not ok_picker or not picker_ui.state or picker_ui.state.mode ~= 'grep' then return end
+
+        local prompt, query = get_prompt_and_query()
+        if query == '' then return end
+
+        local before_cursor = query .. ' **/'
+        local after_cursor = '/**'
+        vim.api.nvim_set_current_line(prompt .. before_cursor .. after_cursor)
+        vim.api.nvim_win_set_cursor(0, { 1, #prompt + #before_cursor })
+      end
+
+      local fff_wrap_group = vim.api.nvim_create_augroup('fff-wrap-query', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        group = fff_wrap_group,
+        pattern = 'fff_input',
+        callback = function(args)
+          local map_opts = { buffer = args.buf, noremap = true, silent = true }
+          vim.keymap.set({ 'i', 'n' }, '<C-i>', add_folder_scope, map_opts)
+        end,
+      })
+    end,
+    keys = {
+      { '<leader>sg', function() require('fff').live_grep() end, desc = '[S]earch by [G]rep' },
+      { '<leader>sw', function() require('fff').live_grep { query = vim.fn.expand '<cword>' } end, mode = { 'n', 'v' }, desc = '[S]earch current [W]ord' },
+      {
+        '<leader>sW',
+        function()
+          local word = vim.fn.expand '<cword>'
+          word = word:gsub('"', '\\"')
+          require('fff').live_grep { query = string.format('"%s" ', word) }
+        end,
+        mode = { 'n', 'v' },
+        desc = '[S]earch current [W]ord (quoted)',
+      },
+    },
+  },
+}
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -92,9 +92,8 @@ return {
       pcall(require('telescope').load_extension, 'live_grep_args')
 
       -- See `:help telescope.builtin`
+      -- Grep is handled by fff.nvim (see fff.lua), file finding stays here
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set(
         'n',
@@ -102,9 +101,9 @@ return {
         function() builtin.find_files { hidden = true, no_ignore = true } end,
         { desc = '[S]earch all [F]iles (incl. ignored)' }
       )
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
